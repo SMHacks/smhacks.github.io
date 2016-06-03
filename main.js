531a61f7793cdfdaa9e93767359bc0893c74351e
saescapa <samuel.escapa@gmail.com>
2016-06-03 10:00:40 -0700

    SM Hacks 2.1

--- main.js
+++ main.js
@@ -1,21 +1,123 @@
 $('.loading').css('display','inline-block')
-$('.bg').css('display','none')
 $('nav').data('offset-top',$(window).height())
 $('html,body').css('overflow-y','hidden')
 $(window).width()<768&&($('.scrollme').removeClass('scrollme'),$('.animateme').removeClass('animateme'))
 $(window).load(function(){
-  $('.bg').css($(window).width()/$(window).height()>1830/1027?{width:'100%',height:'auto'}:{width:'auto',height:'100%'})
+  particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 1000
+        }
+      },
+      "color": {
+        "value": "#fefefe"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 11
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#fefefe",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": true,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 140,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  })
 	$('.circle').delay(500).animate({
 		height:'2000px',
 		width:'2000px',
-		opacity:1,
-		backgroundColor:'#ccc'
+		opacity:1
 	},1000).promise().done(function(){
 		scrollTo(0,0)
 		$('.loading').fadeOut(1000).promise().done(function(){
-      $('.bg').fadeIn(1000).promise().done(function(){
-        $('html,body').css('overflow-y','auto')
-      })
+      $('html,body').css('overflow-y','auto')
 		})
 	})
   $('a[href*="#"]:not([href="#"])').click(function(){
@@ -38,6 +140,6 @@ $(window).load(function(){
       }).delay(4000).slideUp(200)
     })
   })
-}).resize(function(){
-  $('.bg').css($(window).width()/$(window).height()>1830/1027?{width:'100%',height:'auto'}:{width:'auto',height:'100%'})
+}).scroll(function(){
+  $('.bg').css('top','-'+(pageYOffset*.1)+'px')
 })
